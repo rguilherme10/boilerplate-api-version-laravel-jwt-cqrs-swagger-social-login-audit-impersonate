4f8b7ab426aa0355793088644746e4a77d7a766f
Ribamar <ribamargsantos@hotmail.com>
2025-08-21 08:12:08 -0400

    fix ChatSession model

--- app/Models/ChatSession.php
+++ app/Models/ChatSession.php
@@ -1,15 +1,26 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ChatSession extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'user_id',
+        'session_id',
+        'messages',
+    ];
+
     protected $casts = [
         'messages' => 'array',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
