c679f5ebdb3115378c7620c923154611610192f7
Ribamar <ribamargsantos@hotmail.com>
2025-08-21 15:34:29 -0400

    adjust bus call

--- app/Application/Buses/CommandBusInterface.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace App\Application\Buses;
-
-interface CommandBusInterfacea {
-    public function send(object $command): mixed;
-}
--- app/Application/Buses/QueryBusInterface.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace App\Application\Buses;
-
-interface QueryBusInterfacea {
-    public function ask(object $query): mixed;
-}
